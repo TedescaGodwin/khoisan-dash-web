2f4ccab403e8607dbd48a68d70f629788cd30cbd
Tarzesher <tarzesher@gmail.com>
2023-08-01 21:28:14 +0200

    Remove card settings

--- app/(admin)/settings/page.jsx
+++ app/(admin)/settings/page.jsx
@@ -2,7 +2,7 @@
 
 // components
 
-import CardSettings from "components/Cards/CardSettings.js";
+//import CardSettings from "components/Cards/CardSettings.js";
 import CardProfile from "components/Cards/CardProfile.js";
 
 // layout for page
@@ -12,7 +12,7 @@ export default function Settings() {
     <>
       <div className="flex flex-wrap">
         <div className="w-full lg:w-8/12 px-4">
-          <CardSettings />
+          {/* <CardSettings /> */}
         </div>
         <div className="w-full lg:w-4/12 px-4">
           <CardProfile />
